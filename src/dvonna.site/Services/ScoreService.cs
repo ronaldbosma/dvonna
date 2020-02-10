d1803ea5596e8efd59455335b9504b4bc865da20
Ronald Bosma <ronald.bosma@infosupport.com>
2020-02-10 19:59:25 +0100

    Test data bases on real data from dvonna

--- src/dvonna.site/Services/ScoreService.cs
+++ src/dvonna.site/Services/ScoreService.cs
@@ -15,7 +15,7 @@ namespace dvonna.Site.Services
             {
                 Id = 7,
                 Position = 1,
-                Name = "Piet",
+                Name = "Sara",
                 PointsWon = 30,
                 PointsLost = 11,
                 NumberOfGamesPlayed = 12
@@ -24,7 +24,7 @@ namespace dvonna.Site.Services
             {
                 Id = 1,
                 Position = 2,
-                Name = "Hendrik",
+                Name = "Tom",
                 PointsWon = 27,
                 PointsLost = 9,
                 NumberOfGamesPlayed = 9
@@ -33,7 +33,7 @@ namespace dvonna.Site.Services
             {
                 Id = 2,
                 Position = 3,
-                Name = "Linda",
+                Name = "Pete",
                 PointsWon = 26,
                 PointsLost = 13,
                 NumberOfGamesPlayed = 10
@@ -42,7 +42,7 @@ namespace dvonna.Site.Services
             {
                 Id = 6,
                 Position = 4,
-                Name = "Gerard",
+                Name = "Tamara",
                 PointsWon = 21,
                 PointsLost = 5,
                 NumberOfGamesPlayed = 8
@@ -51,7 +51,7 @@ namespace dvonna.Site.Services
             {
                 Id = 20,
                 Position = 5,
-                Name = "Edna",
+                Name = "Rose",
                 PointsWon = 12,
                 PointsLost = 20,
                 NumberOfGamesPlayed = 7
@@ -60,7 +60,7 @@ namespace dvonna.Site.Services
             {
                 Id = 12,
                 Position = 6,
-                Name = "Tony",
+                Name = "Justin",
                 PointsWon = 12,
                 PointsLost = 24,
                 NumberOfGamesPlayed = 10
@@ -69,7 +69,7 @@ namespace dvonna.Site.Services
             {
                 Id = 3,
                 Position = 7,
-                Name = "Tamara",
+                Name = "Hendrik",
                 PointsWon = 11,
                 PointsLost = 7,
                 NumberOfGamesPlayed = 5
@@ -78,7 +78,7 @@ namespace dvonna.Site.Services
             {
                 Id = 15,
                 Position = 8,
-                Name = "Erica",
+                Name = "Harold",
                 PointsWon = 10,
                 PointsLost = 14,
                 NumberOfGamesPlayed = 6
@@ -87,7 +87,7 @@ namespace dvonna.Site.Services
             {
                 Id = 14,
                 Position = 9,
-                Name = "Tom",
+                Name = "Johhny Q.",
                 PointsWon = 10,
                 PointsLost = 15,
                 NumberOfGamesPlayed = 7
@@ -96,10 +96,82 @@ namespace dvonna.Site.Services
             {
                 Id = 21,
                 Position = 10,
-                Name = "Jason",
+                Name = "Carlos",
                 PointsWon = 7,
                 PointsLost = 11,
                 NumberOfGamesPlayed = 5
+            },
+            new PlayerScore
+            {
+                Id = 13,
+                Position = 11,
+                Name = "Johnny H.",
+                PointsWon = 7,
+                PointsLost = 20,
+                NumberOfGamesPlayed = 7
+            },
+            new PlayerScore
+            {
+                Id = 4,
+                Position = 12,
+                Name = "Lara",
+                PointsWon = 6,
+                PointsLost = 1,
+                NumberOfGamesPlayed = 2
+            },
+            new PlayerScore
+            {
+                Id = 8,
+                Position = 13,
+                Name = "Anton",
+                PointsWon = 5,
+                PointsLost = 15,
+                NumberOfGamesPlayed = 6
+            },
+            new PlayerScore
+            {
+                Id = 16,
+                Position = 14,
+                Name = "Sean",
+                PointsWon = 3,
+                PointsLost = 9,
+                NumberOfGamesPlayed = 3
+            },
+            new PlayerScore
+            {
+                Id = 17,
+                Position = 15,
+                Name = "Rosalind",
+                PointsWon = 2,
+                PointsLost = 12,
+                NumberOfGamesPlayed = 4
+            },
+            new PlayerScore
+            {
+                Id = 10,
+                Position = 16,
+                Name = "Jerry",
+                PointsWon = 0,
+                PointsLost = 0,
+                NumberOfGamesPlayed = 0
+            },
+            new PlayerScore
+            {
+                Id = 23,
+                Position = 17,
+                Name = "Edna",
+                PointsWon = 0,
+                PointsLost = 0,
+                NumberOfGamesPlayed = 0
+            },
+            new PlayerScore
+            {
+                Id = 5,
+                Position = 18,
+                Name = "Jason",
+                PointsWon = 0,
+                PointsLost = 3,
+                NumberOfGamesPlayed = 1
             }
         };
 
