06736aa225713b2844ab11e417926136185014d9
Ronald Bosma <ronald.bosma@infosupport.com>
2020-02-10 19:09:45 +0100

    Expanded test data for full score

--- src/dvonna.Shared/PlayerScore.cs
+++ src/dvonna.Shared/PlayerScore.cs
@@ -2,8 +2,16 @@
 {
     public class PlayerScore
     {
+        public int Id { get; set; }
+
         public int Position { get; set; }
 
         public string Name { get; set; }
+
+        public int PointsFor { get; set; }
+
+        public int PointsAgainst { get; set; }
+
+        public int NumberOfGamesPlayed { get; set; }
     }
 }
--- src/dvonna.site/Services/ScoreService.cs
+++ src/dvonna.site/Services/ScoreService.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 using dvonna.Shared;
 
@@ -6,36 +7,108 @@ namespace dvonna.Site.Services
 {
     public class ScoreService : IScoreService
     {
+        #region Test Data
+
+        private static readonly IEnumerable<PlayerScore> _score = new List<PlayerScore>
+        {
+            new PlayerScore
+            {
+                Id = 7,
+                Position = 1,
+                Name = "Piet",
+                PointsFor = 30,
+                PointsAgainst = 11,
+                NumberOfGamesPlayed = 12
+            },
+            new PlayerScore
+            {
+                Id = 1,
+                Position = 2,
+                Name = "Hendrik",
+                PointsFor = 27,
+                PointsAgainst = 9,
+                NumberOfGamesPlayed = 9
+            },
+            new PlayerScore
+            {
+                Id = 2,
+                Position = 3,
+                Name = "Linda",
+                PointsFor = 26,
+                PointsAgainst = 13,
+                NumberOfGamesPlayed = 10
+            },
+            new PlayerScore
+            {
+                Id = 6,
+                Position = 4,
+                Name = "Gerard",
+                PointsFor = 21,
+                PointsAgainst = 5,
+                NumberOfGamesPlayed = 8
+            },
+            new PlayerScore
+            {
+                Id = 20,
+                Position = 5,
+                Name = "Edna",
+                PointsFor = 12,
+                PointsAgainst = 20,
+                NumberOfGamesPlayed = 7
+            },
+            new PlayerScore
+            {
+                Id = 12,
+                Position = 6,
+                Name = "Tony",
+                PointsFor = 12,
+                PointsAgainst = 24,
+                NumberOfGamesPlayed = 10
+            },
+            new PlayerScore
+            {
+                Id = 3,
+                Position = 7,
+                Name = "Tamara",
+                PointsFor = 11,
+                PointsAgainst = 7,
+                NumberOfGamesPlayed = 5
+            },
+            new PlayerScore
+            {
+                Id = 15,
+                Position = 8,
+                Name = "Erica",
+                PointsFor = 10,
+                PointsAgainst = 14,
+                NumberOfGamesPlayed = 6
+            },
+            new PlayerScore
+            {
+                Id = 14,
+                Position = 9,
+                Name = "Tom",
+                PointsFor = 10,
+                PointsAgainst = 15,
+                NumberOfGamesPlayed = 7
+            },
+            new PlayerScore
+            {
+                Id = 21,
+                Position = 10,
+                Name = "Jason",
+                PointsFor = 7,
+                PointsAgainst = 11,
+                NumberOfGamesPlayed = 5
+            }
+        };
+
+        #endregion
+
         public Task<IEnumerable<PlayerScore>> GetTop5Async()
         {
-            return Task.FromResult<IEnumerable<PlayerScore>>(new List<PlayerScore>
-            {
-                new PlayerScore
-                {
-                    Position = 1,
-                    Name = "Piet"
-                },
-                new PlayerScore
-                {
-                    Position = 2,
-                    Name = "Hendrik"
-                },
-                new PlayerScore
-                {
-                    Position = 3,
-                    Name = "Linda"
-                },
-                new PlayerScore
-                {
-                    Position = 4,
-                    Name = "Gerard"
-                },
-                new PlayerScore
-                {
-                    Position = 5,
-                    Name = "Edna"
-                }
-            });
+            var top5 = _score.OrderBy(s => s.Position).Take(5);
+            return Task.FromResult<IEnumerable<PlayerScore>>(top5);
         }
     }
 }
