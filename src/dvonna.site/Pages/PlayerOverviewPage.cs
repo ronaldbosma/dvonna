992310e4f00405931bea9a197bd6112f5ce6532b
Ronald Bosma <ronald.bosma@infosupport.com>
2020-02-25 19:05:09 +0100

    order players by name

--- src/dvonna.site/Pages/PlayerOverviewPage.cs
+++ src/dvonna.site/Pages/PlayerOverviewPage.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 using dvonna.Site.Services;
 using Microsoft.AspNetCore.Components;
@@ -13,14 +14,20 @@ namespace dvonna.Site.Pages
         [Inject]
         public IPlayerPreference PlayerPreference { get; set; }
 
-        public IDictionary<int, string> Players { get; set; }
+        public IOrderedEnumerable<KeyValuePair<int, string>> Players { get; set; }
 
         public string SelectedPlayerId { get; set; }
 
         protected override async Task OnInitializedAsync()
         {
-            Players = await PlayerService.GetPlayersAsync();
+            Players = await GetPlayersOrderedByNameAsync();
             SelectedPlayerId = await LoadSelectedPlayerIdAsync();
+        }
+
+        private async Task<IOrderedEnumerable<KeyValuePair<int, string>>> GetPlayersOrderedByNameAsync()
+        {
+            var players = await PlayerService.GetPlayersAsync();
+            return players.OrderBy(p => p.Value);
         }
 
         private async Task<string> LoadSelectedPlayerIdAsync()
