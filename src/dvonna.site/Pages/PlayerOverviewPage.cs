0006915ee3a0fe65304c6e40ecf749af42001adc
Ronald Bosma <ronald.bosma@infosupport.com>
2020-02-25 18:37:49 +0100

    Dropdown to select player

--- src/dvonna.site/Pages/PlayerOverviewPage.cs
+++ src/dvonna.site/Pages/PlayerOverviewPage.cs
@@ -2,11 +2,23 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using dvonna.Site.Services;
 using Microsoft.AspNetCore.Components;
 
 namespace dvonna.Site.Pages
 {
     public class PlayerOverviewPage : ComponentBase
     {
+        [Inject]
+        public IPlayerService PlayerService { get; set; }
+
+        public IDictionary<int, string> Players { get; set; }
+
+        public string SelectedPlayerId { get; set; }
+
+        protected override async Task OnInitializedAsync()
+        {
+            Players = await PlayerService.GetPlayersAsync();
+        }
     }
 }
