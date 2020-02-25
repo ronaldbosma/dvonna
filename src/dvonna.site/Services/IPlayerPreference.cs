a7d20714a15492b343ce0730494509fa24f38075
Ronald Bosma <ronald.bosma@infosupport.com>
2020-02-25 18:53:44 +0100

    Class to get and store preferred player

---
+++ src/dvonna.site/Services/IPlayerPreference.cs
@@ -0,0 +1,20 @@
+﻿using System.Threading.Tasks;
+
+namespace dvonna.Site.Services
+{
+    /// <summary>
+    /// Can be used to retrieve and store a preferred player.
+    /// </summary>
+    public interface IPlayerPreference
+    {
+        /// <summary>
+        /// Gets the saved player id if found, else null.
+        /// </summary>
+        Task<int?> GetSavedPlayerIdAsync();
+
+        /// <summary>
+        /// Saved <paramref name="playerId"/> as the preferred player.
+        /// </summary>
+        Task SavePlayerIdAsync(int playerId);
+    }
+}
---
+++ src/dvonna.site/Services/PlayerPreference.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace dvonna.Site.Services
+{
+    public class PlayerPreference : IPlayerPreference
+    {
+        private int? _savedPlayerId = 17;
+        private readonly IPlayerService _playerService;
+
+        public PlayerPreference(IPlayerService playerService)
+        {
+            _playerService = playerService ?? throw new ArgumentNullException(nameof(playerService));
+        }
+
+        public async Task<int?> GetSavedPlayerIdAsync()
+        {
+            var players = await _playerService.GetPlayersAsync();
+            var savedPlayerIsExistingPlayer = _savedPlayerId.HasValue && players.ContainsKey(_savedPlayerId.Value);
+
+            return savedPlayerIsExistingPlayer ? _savedPlayerId : null;
+        }
+
+        public Task SavePlayerIdAsync(int playerId)
+        {
+            _savedPlayerId = playerId;
+            return Task.CompletedTask;
+        }
+    }
+}
