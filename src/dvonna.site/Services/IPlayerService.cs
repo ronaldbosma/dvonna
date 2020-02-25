f5703bd7705cab799a846b1418b371fd631a7948
Ronald Bosma <ronald.bosma@infosupport.com>
2020-02-25 17:47:32 +0100

    Player Service with test data

---
+++ src/dvonna.site/Services/IPlayerService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace dvonna.Site.Services
+{
+    public interface IPlayerService
+    {
+        Task<IDictionary<int, string>> GetPlayersAsync();
+    }
+}
---
+++ src/dvonna.site/Services/PlayerService.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace dvonna.Site.Services
+{
+    public class PlayerService : IPlayerService
+    {
+        public Task<IDictionary<int, string>> GetPlayersAsync()
+        {
+            return Task.FromResult<IDictionary<int, string>>(new Dictionary<int, string>
+            {
+                { 1, "Tom" },
+                { 2, "Pete" },
+                { 3, "Hendrik" },
+                { 4, "Lara" },
+                { 5, "Jason" },
+                { 6, "Tamara" },
+                { 7, "Sara" },
+                { 8, "Anton" },
+                { 10, "Jerry" },
+                { 12, "Justing" },
+                { 13, "Johnny H." },
+                { 14, "Johhny Q." },
+                { 15, "Harold" },
+                { 16, "Sean" },
+                { 17, "Rosalind" },
+                { 20, "Rose" },
+                { 23, "Edna" },
+                { 21, "Carlos" }
+            });
+        }
+    }
+}
