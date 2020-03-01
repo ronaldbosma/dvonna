41850918b376208ef7348e91fa4967274eba3f60
Ronald Bosma <ronald.bosma@infosupport.com>
2020-03-01 11:02:25 +0100

    Use player details to return list of players

---
+++ src/dvonna.Shared/PlayerDetails.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace dvonna.Shared
+{
+    public class PlayerDetails
+    {
+        public int Id { get; set; }
+
+        public int Position { get; set; }
+
+        public string Name { get; set; }
+
+        public int PointsWon { get; set; }
+
+        public int PointsLost { get; set; }
+
+        public int NumberOfGamesPlayed { get; set; }
+    }
+}
--- src/dvonna.site/Services/PlayerService.cs
+++ src/dvonna.site/Services/PlayerService.cs
@@ -1,33 +1,181 @@
 ﻿using System.Collections.Generic;
 using System.Threading.Tasks;
+using dvonna.Shared;
+using System.Linq;
 
 namespace dvonna.Site.Services
 {
     public class PlayerService : IPlayerService
     {
+        private static readonly IEnumerable<PlayerDetails> _playerDetails = new List<PlayerDetails>
+        {
+            new PlayerDetails
+            {
+                Id = 7,
+                Position = 1,
+                Name = "Sara",
+                PointsWon = 30,
+                PointsLost = 11,
+                NumberOfGamesPlayed = 12
+            },
+            new PlayerDetails
+            {
+                Id = 1,
+                Position = 2,
+                Name = "Tom",
+                PointsWon = 27,
+                PointsLost = 9,
+                NumberOfGamesPlayed = 9
+            },
+            new PlayerDetails
+            {
+                Id = 2,
+                Position = 3,
+                Name = "Pete",
+                PointsWon = 26,
+                PointsLost = 13,
+                NumberOfGamesPlayed = 10
+            },
+            new PlayerDetails
+            {
+                Id = 6,
+                Position = 4,
+                Name = "Tamara",
+                PointsWon = 21,
+                PointsLost = 5,
+                NumberOfGamesPlayed = 8
+            },
+            new PlayerDetails
+            {
+                Id = 20,
+                Position = 5,
+                Name = "Rose",
+                PointsWon = 12,
+                PointsLost = 20,
+                NumberOfGamesPlayed = 7
+            },
+            new PlayerDetails
+            {
+                Id = 12,
+                Position = 6,
+                Name = "Justin",
+                PointsWon = 12,
+                PointsLost = 24,
+                NumberOfGamesPlayed = 10
+            },
+            new PlayerDetails
+            {
+                Id = 3,
+                Position = 7,
+                Name = "Hendrik",
+                PointsWon = 11,
+                PointsLost = 7,
+                NumberOfGamesPlayed = 5
+            },
+            new PlayerDetails
+            {
+                Id = 15,
+                Position = 8,
+                Name = "Harold",
+                PointsWon = 10,
+                PointsLost = 14,
+                NumberOfGamesPlayed = 6
+            },
+            new PlayerDetails
+            {
+                Id = 14,
+                Position = 9,
+                Name = "Johhny Q.",
+                PointsWon = 10,
+                PointsLost = 15,
+                NumberOfGamesPlayed = 7
+            },
+            new PlayerDetails
+            {
+                Id = 21,
+                Position = 10,
+                Name = "Carlos",
+                PointsWon = 7,
+                PointsLost = 11,
+                NumberOfGamesPlayed = 5
+            },
+            new PlayerDetails
+            {
+                Id = 13,
+                Position = 11,
+                Name = "Johnny H.",
+                PointsWon = 7,
+                PointsLost = 20,
+                NumberOfGamesPlayed = 7
+            },
+            new PlayerDetails
+            {
+                Id = 4,
+                Position = 12,
+                Name = "Lara",
+                PointsWon = 6,
+                PointsLost = 1,
+                NumberOfGamesPlayed = 2
+            },
+            new PlayerDetails
+            {
+                Id = 8,
+                Position = 13,
+                Name = "Anton",
+                PointsWon = 5,
+                PointsLost = 15,
+                NumberOfGamesPlayed = 6
+            },
+            new PlayerDetails
+            {
+                Id = 16,
+                Position = 14,
+                Name = "Sean",
+                PointsWon = 3,
+                PointsLost = 9,
+                NumberOfGamesPlayed = 3
+            },
+            new PlayerDetails
+            {
+                Id = 17,
+                Position = 15,
+                Name = "Rosalind",
+                PointsWon = 2,
+                PointsLost = 12,
+                NumberOfGamesPlayed = 4
+            },
+            new PlayerDetails
+            {
+                Id = 10,
+                Position = 16,
+                Name = "Jerry",
+                PointsWon = 0,
+                PointsLost = 0,
+                NumberOfGamesPlayed = 0
+            },
+            new PlayerDetails
+            {
+                Id = 23,
+                Position = 17,
+                Name = "Edna",
+                PointsWon = 0,
+                PointsLost = 0,
+                NumberOfGamesPlayed = 0
+            },
+            new PlayerDetails
+            {
+                Id = 5,
+                Position = 18,
+                Name = "Jason",
+                PointsWon = 0,
+                PointsLost = 3,
+                NumberOfGamesPlayed = 1
+            }
+        };
+
         public Task<IDictionary<int, string>> GetPlayersAsync()
         {
-            return Task.FromResult<IDictionary<int, string>>(new Dictionary<int, string>
-            {
-                { 1, "Tom" },
-                { 2, "Pete" },
-                { 3, "Hendrik" },
-                { 4, "Lara" },
-                { 5, "Jason" },
-                { 6, "Tamara" },
-                { 7, "Sara" },
-                { 8, "Anton" },
-                { 10, "Jerry" },
-                { 12, "Justin" },
-                { 13, "Johnny H." },
-                { 14, "Johhny Q." },
-                { 15, "Harold" },
-                { 16, "Sean" },
-                { 17, "Rosalind" },
-                { 20, "Rose" },
-                { 23, "Edna" },
-                { 21, "Carlos" }
-            });
+            return Task.FromResult<IDictionary<int, string>>(_playerDetails.ToDictionary(p => p.Id, p => p.Name));
         }
     }
 }
