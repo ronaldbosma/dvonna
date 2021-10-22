90a1959ddb424cde3ad1d275af6445fd20e74ee6
Ronald Bosma <ronald.bosma@infosupport.com>
2021-10-22 17:22:12 +0200

    Update agenda test data to 2021-2022

--- src/dvonna.site/Services/AgendaService.cs
+++ src/dvonna.site/Services/AgendaService.cs
@@ -11,15 +11,15 @@ namespace dvonna.Site.Services
         {
             return Task.FromResult<IEnumerable<PlayDate>>(new List<PlayDate>
             {
-                new PlayDate { Date = new DateTime(2019, 9, 13) },
-                new PlayDate { Date = new DateTime(2019, 10, 11) },
-                new PlayDate { Date = new DateTime(2019, 11, 8) },
-                new PlayDate { Date = new DateTime(2019, 12, 13) },
-                new PlayDate { Date = new DateTime(2020, 1, 10) },
-                new PlayDate { Date = new DateTime(2020, 2, 14) },
-                new PlayDate { Date = new DateTime(2020, 3, 13) },
-                new PlayDate { Date = new DateTime(2020, 4, 10) },
-                new PlayDate { Date = new DateTime(2020, 5, 8), Comments = "BBQ, aanvang 6 uur" },
+                new PlayDate { Date = new DateTime(2021, 9, 13) },
+                new PlayDate { Date = new DateTime(2021, 10, 11) },
+                new PlayDate { Date = new DateTime(2021, 11, 8) },
+                new PlayDate { Date = new DateTime(2021, 12, 13) },
+                new PlayDate { Date = new DateTime(2022, 1, 10) },
+                new PlayDate { Date = new DateTime(2022, 2, 14) },
+                new PlayDate { Date = new DateTime(2022, 3, 13) },
+                new PlayDate { Date = new DateTime(2022, 4, 10) },
+                new PlayDate { Date = new DateTime(2022, 5, 8), Comments = "BBQ, aanvang 6 uur" },
             });
         }
     }
