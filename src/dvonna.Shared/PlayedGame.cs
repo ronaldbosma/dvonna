8643d6b5ecd704b7c181c03e7438b864b116e464
Ronald Bosma <ronald.bosma@infosupport.com>
2021-10-22 16:56:49 +0200

    Add played games to player details

---
+++ src/dvonna.Shared/PlayedGame.cs
@@ -0,0 +1,11 @@
+﻿namespace dvonna.Shared
+{
+    public class PlayedGame
+    {
+        public Opponent Opponent { get; set; }
+
+        public int PointsWon { get; set; }
+
+        public int PointsLost { get; set; }
+    }
+}
--- src/dvonna.Shared/PlayerDetails.cs
+++ src/dvonna.Shared/PlayerDetails.cs
@@ -7,6 +7,7 @@ namespace dvonna.Shared
         public PlayerDetails()
         {
             OpponentToPlay = new List<Opponent>();
+            PlayedGames = new List<PlayedGame>();
         }
 
         public int Id { get; set; }
@@ -15,12 +16,16 @@ namespace dvonna.Shared
 
         public string Name { get; set; }
 
+        //TODO: rename tot TotalPointsWon
         public int PointsWon { get; set; }
 
+        //TODO: rename tot TotalPointsLost
         public int PointsLost { get; set; }
 
         public int NumberOfGamesPlayed { get; set; }
 
         public IEnumerable<Opponent> OpponentToPlay { get; set; }
+
+        public IEnumerable<PlayedGame> PlayedGames { get; set; }
     }
 }
