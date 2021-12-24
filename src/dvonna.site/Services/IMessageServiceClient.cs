857a80e7b1fb787dffa79f7b8c3d97d42e3fda28
Ronald Bosma <ronald.bosma@infosupport.com>
2021-12-24 12:03:46 +0100

    Add a message service client with sample data

---
+++ src/dvonna.site/Services/IMessageServiceClient.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using dvonna.Shared;
+
+namespace dvonna.Site.Services
+{
+    public interface IMessageServiceClient
+    {
+        Task<IEnumerable<Message>> GetMessagesAsync();
+    }
+}
