67a4cb4170bbc232a9f9d109af4154ae34466caf
Ronald Bosma <ronald.bosma@infosupport.com>
2021-12-24 12:05:54 +0100

    Add messages page

---
+++ src/dvonna.site/Pages/MessagesPage.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using dvonna.Shared;
+using dvonna.Site.Services;
+using Microsoft.AspNetCore.Components;
+
+namespace dvonna.Site.Pages
+{
+    public class MessagesPage : ComponentBase
+    {
+        [Inject]
+        public IMessageService MessageService { get; set; }
+
+        public IEnumerable<Message> Messages { get; set; }
+
+        protected override async Task OnInitializedAsync()
+        {
+            var messages = await MessageService.GetMessagesAsync();
+            Messages = messages.OrderByDescending(m => m.CreatedOn).OrderByDescending(m => m.Id);
+        }
+    }
+}
