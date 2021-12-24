7286ef716d07c25846464b50ada6a6272f92df89
Ronald Bosma <ronald.bosma@infosupport.com>
2021-12-24 12:44:23 +0100

    Add message alerts to home screen.

---
+++ src/dvonna.site/Components/MessageAlertsComponent.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using dvonna.Shared;
+using dvonna.Site.Services;
+using Microsoft.AspNetCore.Components;
+
+namespace dvonna.Site.Components
+{
+    public class MessageAlertsComponent : ComponentBase
+    {
+        [Inject]
+        public IMessageService MessageService { get; set; }
+
+        public IEnumerable<Message> Messages { get; set; }
+
+        protected override async Task OnInitializedAsync()
+        {
+            await LoadMessagesAsync();
+        }
+
+        public async Task MarkMessageAsReadAsync(int id)
+        {
+            await MessageService.MarkMessageAsRead(id);
+            await LoadMessagesAsync();
+        }
+
+        private async Task LoadMessagesAsync()
+        {
+            var messages = await MessageService.GetActiveUnreadMessagesAsync();
+            Messages = messages.OrderByDescending(m => m.CreatedOn).OrderByDescending(m => m.Id);
+        }
+    }
+}
