e1921fc2e8c984e52fcc40983d9138b7f99a6a1a
Ronald Bosma <ronald.bosma@infosupport.com>
2021-12-24 12:04:57 +0100

    Add message object

---
+++ src/dvonna.Shared/Message.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace dvonna.Shared
+{
+    public class Message
+    {
+        public int Id { get; set; }
+
+        public string Content { get; set; }
+
+        public DateTime CreatedOn { get; set; }
+
+        public DateTime ShowUntilDate { get; set; }
+    }
+}
